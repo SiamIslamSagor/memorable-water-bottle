d3bc71fe6498ce4b15f4fe0c656f334299e48dac
SiamIslamSagor <msiamislam12345@gmail.com>
2023-09-12 19:06:50 +0600

    complet project

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -2,6 +2,12 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import "./Bottles.css";
+import {
+  addToLS,
+  getStoredCart,
+  removeFromLS,
+} from "../../utilities/localstorage";
+import Cart from "../Cart/Cart";
 
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
@@ -14,15 +20,47 @@ const Bottles = () => {
       .then((data) => setBottles(data));
   }, []);
 
+  // load cart from local storage
+  useEffect(() => {
+    // console.log("called the useEffect", bottles.length);
+    if (bottles.length > 0) {
+      const storedCart = getStoredCart();
+      // console.log(storedCart, bottles);
+
+      const savedCart = [];
+      for (const id of storedCart) {
+        // console.log(id);
+        const bottle = bottles.find((bottle) => bottle.id === id);
+        if (bottle) {
+          savedCart.push(bottle);
+          // console.log(bottle);
+        }
+      }
+      console.log("savedCart", savedCart);
+      setCart(savedCart);
+    }
+  }, [bottles]);
+
   const handleAddToCart = (bottle) => {
     const newCart = [...cart, bottle];
     setCart(newCart);
+    addToLS(bottle.id);
+  };
+
+  const handleRemoveFromCart = (id) => {
+    //  remove from visual cart remove
+    const remainingCart = cart.filter((bottle) => bottle.id !== id);
+    setCart(remainingCart);
+    // remove from LocalStorage
+    removeFromLS(id);
   };
 
   return (
     <div>
-      <h2>Bottles Available: {bottles.length}</h2>
-      <h4>Cart: {cart.length}</h4>
+      <h2>Bottels vAailable: {bottles.length}</h2>
+
+      <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
+
       <div className="bottle-container">
         {bottles.map((bottle) => (
           <Bottle
